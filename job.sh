0ade826131c20f438cb10cf020ca4364784fb4db
lidq92 <sglidq625@gmail.com>
2019-11-08 02:26:13 +0800

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # nohup ./job.sh > CNNIQA-LIVE-0-10.log 2>&1 &
-source activate reproduciableresearch
+source activate reproducibleresearch
 for ((i=0; i<10; i++)); do
     CUDA_VISIBLE_DEVICES=1 python main.py --exp_id=$i --database=LIVE
 done;
